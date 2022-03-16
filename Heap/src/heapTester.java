5855d4e71917a174cf30271c4bda19180f3ae37d
MuhammadElkotb <Muhammad175ib59@gmail.com>
2022-03-16 16:51:17 +0200

    Heap sort, Insertion, DeleteMin Junit Tests Added

---
+++ Heap/src/heapTester.java
@@ -0,0 +1,77 @@
+import org.junit.*;
+
+import java.lang.annotation.Repeatable;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.PriorityQueue;
+import java.util.Random;
+
+
+
+public class heapTester{
+
+    Heap<Integer> heap = new Heap<>();
+    PriorityQueue<Integer> javaHeap = new PriorityQueue<>((x, y) -> Integer.compare(y, x));
+
+    @Test
+    public void test(){
+        this.testHeapIntegerInsert();
+        this.testHeapIntegerDeleteMin();
+        this.testHeapSort();
+    }
+
+
+    public void testHeapIntegerInsert(){
+
+        Random rand = new Random();
+        rand.setSeed(System.currentTimeMillis());
+        try{
+            for(int i = 0; i < 10000; i++){
+                int randInt = rand.nextInt() % 1000000;
+                heap.insert(randInt);
+                javaHeap.add(randInt);
+            }
+            Assert.assertEquals(heap.toString(), javaHeap.toString());
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+
+    }
+
+    public void testHeapIntegerDeleteMin(){
+
+        for(int i = 0; i < 5000; i++){
+            javaHeap.remove();
+            heap.deleteMin();
+        }
+        Assert.assertEquals(heap.toString(), javaHeap.toString());
+
+
+    }
+
+    public void testHeapSort(){
+
+        ArrayList<Integer> arrayList = new ArrayList<>();
+        ArrayList<Integer> arrayListHeap = new ArrayList<>();
+        Heap<Integer> heapBuild = new Heap<>();
+        Random rand = new Random();
+        rand.setSeed(System.currentTimeMillis());
+        try{
+            for(int i = 0; i < 10000; i++){
+                int randInt = rand.nextInt() % 1000000;
+                arrayList.add(randInt);
+            }
+            arrayList.sort((x, y) -> Integer.compare(y, x));
+            arrayListHeap = heapBuild.HeapSort(arrayList);
+            Assert.assertEquals(arrayListHeap.toString(), arrayList.toString());
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+
+
+
+}
