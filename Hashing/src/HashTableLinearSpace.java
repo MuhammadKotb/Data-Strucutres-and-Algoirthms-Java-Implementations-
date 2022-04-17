9388f1f7d601e8f3565fc8123913fb76697293ed
Deffo0 <73228199+Deffo0@users.noreply.github.com>
2022-04-17 12:46:30 +0200

    QuadraticSpace Hashing

---
+++ Hashing/src/HashTableLinearSpace.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+
+public class HashTableLinearSpace<T extends Comparable<T>>{
+    private ArrayList<T> hashTable;
+    HashTableLinearSpace(int size){
+        this.hashTable = new ArrayList<T>(size);
+
+    }
+    public static int HashFunction(){
+
+    }
+    public void insert(T data) {
+
+    }
+}
